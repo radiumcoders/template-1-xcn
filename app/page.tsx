51c0039cbb1cfe4841dd22c72321c42145b5ab81
Radiumcoders <radiumcoders@gmail.com>
2026-04-26 16:52:52 +0530

    Refactor landing section layout and enhance button styles
    Revise the layout of the landing section by adding a background pattern and adjusting the button styles for better visibility. Update the structure to improve responsiveness and visual appeal, while removing the commented-out image placeholder.

--- app/page.tsx
+++ app/page.tsx
@@ -24,27 +24,30 @@ export default function Home() {
         </Container>
       </nav>
       <section className="h-96 w-full border-b border-border">
-        <Container className="flex h-full flex-col items-center justify-end pb-10 border-x border-border px-2 gap-4">
-          <h1 className="text-center text-5xl sm:text-6xl md:text-7xl">
-            Get Better Reports <br /> With OpenClaw
-          </h1>
-          <div>
-            <Button>Get Started</Button>
-            <Button>View Pricing</Button>
+        <Container className="relative flex h-full flex-col items-center justify-end gap-4 border-x border-border px-2 [--pattern:var(--color-mist-100)] dark:[--pattern:var(--color-mist-900)]">
+          <div className="absolute h-full w-full bg-[repeating-linear-gradient(90deg,var(--pattern)_0,var(--pattern)_1px,transparent_100%,transparent_50%)] bg-size-[40px_40px] mask-b-from-0"></div>
+          <div className="z-10 flex flex-col gap-4 bg-transparent py-10">
+            <h1 className="text-center text-5xl tracking-tighter sm:text-6xl md:text-7xl">
+              Get Better Reports <br /> With OpenClaw
+            </h1>
+            <div className="flex items-center justify-center gap-2">
+              <Button size={"lg"}>Get Started</Button>
+              <Button size={"lg"} variant={"outline"}>
+                View Pricing
+              </Button>
+            </div>
           </div>
         </Container>
       </section>
-      <section className="h-fit w-full border-b border-border">
-        <Container className="border-x border-border">
-          <Image
-            src={"/asset.jpg"}
-            quality={1000}
-            className="rounded-none"
-            alt="hero-image"
-            height={1080}
-            width={1920}
-          />
-        </Container>
+      <section className="flex h-136 w-full items-center justify-center border-b border-border">
+        <div className="h-full w-full max-w-4xl mx-auto border-x border-border [--pattern:var(--color-mist-100)] dark:[--pattern:var(--color-mist-900)]">
+          {/* <div className="flex h-full w-full items-center justify-center bg-[repeating-linear-gradient(315deg,var(--pattern)_0,var(--pattern)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px]">
+            <span className="border border-border bg-background/50 p-2 uppercase backdrop-blur-2xl">
+              image here
+            </span>
+          </div> */}
+          
+        </div>
       </section>
     </main>
   )
